b555fe57571a606355b24f9196e267832d1cd448
G T Thushan <theekshanathushan89@gmail.com>
2026-07-19 08:31:41 +0530

    I add more feaetures in Jobposting.cs file\.I update this

--- backend/RecruitmentPlatform.API/Models/JobPosting.cs
+++ backend/RecruitmentPlatform.API/Models/JobPosting.cs
@@ -1,21 +1,24 @@
 using System;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RecruitmentPlatform.API.Models
 {
     public class JobPosting
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = string.Empty;
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string RequiredSkills { get; set; } = string.Empty;
 
-        [BsonRepresentation(BsonType.ObjectId)]
         public string RecruiterId { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public bool IsActive { get; set; } = true;
+
+        [ForeignKey(nameof(RecruiterId))]
+        public User? Recruiter { get; set; }
+
+        public List<Application> Applications { get; set; } = new();
     }
 }

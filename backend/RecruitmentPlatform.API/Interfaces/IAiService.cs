6ba28e66b9126f79d8b9481c54ce27206902d7f9
G T Thushan <theekshanathushan89@gmail.com>
2026-07-22 09:29:45 +0530

    I add new file in interface folder IAiService.cs file

---
+++ backend/RecruitmentPlatform.API/Interfaces/IAiService.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RecruitmentPlatform.API.DTOs;
+using RecruitmentPlatform.API.Models;
+
+namespace RecruitmentPlatform.API.Interfaces
+{
+    public interface IAiService
+    {
+        Task<ParsedResumeDto> ParseResumeAsync(string resumeText);
+        Task<MatchResultDto> MatchCandidateToJobAsync(User candidate, JobPosting job);
+        Task<string> AnalyzeHiringTrendsAsync(List<JobPosting> jobs, List<Application> applications, List<Department> departments);
+        Task<List<JobRecommendationResultDto>> GetJobRecommendationsAsync(User candidate, List<JobPosting> activeJobs);
+        Task<string> GenerateFeedbackAsync(User candidate, JobPosting job, Application application, int evaluationScore);
+        Task<string> ConductLiveInterviewAsync(JobPosting job, User candidate, List<ChatMessageDto> chatHistory, string newAnswer);
+    }
+}
